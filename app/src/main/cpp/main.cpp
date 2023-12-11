d78e96045532023103715532daa09ba39f098e0f
git-for-jobs-123 <->
2023-12-11 17:00:38 +0200

    Remove unnecessary globals file

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -9,7 +9,25 @@
 
 using namespace cv;
 
-#include "globals.h"
+enum PreviewMode {
+  DETECT_PREVIEW_MOTION_WHITE,
+  DETECT_PREVIEW_MOTION_RED,
+  DETECT_PREVIEW_MOTION_GREEN,
+  DETECT_PREVIEW_MOTION_BLUE,
+  DETECT_PREVIEW_MOTION_GRAYSCALE,
+  DETECT_PREVIEW_MOTION_WHITE_WITH_BACKGROUND,
+  DETECT_MOTION_LINES,
+};
+
+PreviewMode previewMode;
+PreviewMode previousPreviewMode;
+
+GLuint ibo;
+GLuint vbo;
+
+int cameraWidth;
+int cameraHeight;
+
 #include "detector.cpp"
 #include "detector_motion.cpp"
 #include "detector_motion_image.cpp"
